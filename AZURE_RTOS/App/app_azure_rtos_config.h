ca4b3a497c3d004faed57707fa258612433f6082
Jens <jensboe@outlook.de>
2025-08-05 19:45:06 +0200

    remove 3rd party code (#9)
    * Add ThreadX support and configure timer (#3)
    
    * Add ThreadX in H5 cooking
    
    * Replace stm32h5xx-hal-driver with git submodule fix #5
    
    * Remove device specific cmsis files. fix #7
    
    * Replace CMSIS with git submodule fix #6
    
    * Use TheadX as git submodule
    
    * Use threadx own cmake file
    
    * Remove unused defines

--- AZURE_RTOS/App/app_azure_rtos_config.h
+++ AZURE_RTOS/App/app_azure_rtos_config.h
@@ -1,8 +1,10 @@
+
 /* USER CODE BEGIN Header */
 /**
   ******************************************************************************
-  * @file    stm32h5xx_it.h
-  * @brief   This file contains the headers of the interrupt handlers.
+  * @file    app_azure_rtos_config.h
+  * @author  MCD Application Team
+  * @brief   app_azure_rtos config header file
   ******************************************************************************
   * @attention
   *
@@ -13,17 +15,18 @@
   * in the root directory of this software component.
   * If no LICENSE file comes with this software, it is provided AS-IS.
   *
- ******************************************************************************
+  ******************************************************************************
   */
 /* USER CODE END Header */
 
 /* Define to prevent recursive inclusion -------------------------------------*/
-#ifndef __STM32H5xx_IT_H
-#define __STM32H5xx_IT_H
-
+#ifndef APP_AZURE_RTOS_CONFIG_H
+#define APP_AZURE_RTOS_CONFIG_H
 #ifdef __cplusplus
- extern "C" {
+extern "C" {
 #endif
+
+/* Includes ------------------------------------------------------------------*/
 
 /* Private includes ----------------------------------------------------------*/
 /* USER CODE BEGIN Includes */
@@ -36,6 +39,12 @@
 /* USER CODE END ET */
 
 /* Exported constants --------------------------------------------------------*/
+/* Using static memory allocation via threadX Byte memory pools */
+
+#define USE_STATIC_ALLOCATION                1
+
+#define TX_APP_MEM_POOL_SIZE                     2*1024
+
 /* USER CODE BEGIN EC */
 
 /* USER CODE END EC */
@@ -46,20 +55,18 @@
 /* USER CODE END EM */
 
 /* Exported functions prototypes ---------------------------------------------*/
-void NMI_Handler(void);
-void HardFault_Handler(void);
-void MemManage_Handler(void);
-void BusFault_Handler(void);
-void UsageFault_Handler(void);
-void DebugMon_Handler(void);
-void EXTI13_IRQHandler(void);
-void TIM1_UP_IRQHandler(void);
 /* USER CODE BEGIN EFP */
 
 /* USER CODE END EFP */
+
+/* Private defines -----------------------------------------------------------*/
+/* USER CODE BEGIN PD */
+
+/* USER CODE END PD */
 
 #ifdef __cplusplus
 }
 #endif
 
-#endif /* __STM32H5xx_IT_H */
+#endif /* APP_AZURE_RTOS_CONFIG_H */
+
