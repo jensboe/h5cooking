81488d8acb841efdfb1242ab83e873651553190e
Jens Böckmann <jensboe@outlook.de>
2025-08-07 00:01:31 +0200

    Add blinky green

---
+++ apps/blinky_green/main.c
@@ -0,0 +1,13 @@
+#include "stm32h5xx_hal.h"
+#include "stm32h5xx_nucleo.h"
+#include <stdio.h>
+#include "appmain.h"
+
+int app_main(void)
+{
+    for(;;)
+    {
+        HAL_GPIO_TogglePin(LED1_GPIO_PORT, LED1_PIN);
+        HAL_Delay(500);
+    }
+}
