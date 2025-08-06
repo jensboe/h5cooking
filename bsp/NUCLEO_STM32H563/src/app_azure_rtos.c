fa395f0d1acd4b7bf9496eacf79dab2097bddf71
Jens Böckmann <jensboe@outlook.de>
2025-08-06 23:46:13 +0200

    Refactor syscalls.c for consistent formatting and indentation
    - Adjusted indentation for better readability across all functions.
    - Removed unnecessary blank lines to streamline the code.
    - Ensured consistent use of braces for single-line statements.

--- bsp/NUCLEO_STM32H563/src/app_azure_rtos.c
+++ bsp/NUCLEO_STM32H563/src/app_azure_rtos.c
@@ -1,54 +1,9 @@
-/* USER CODE BEGIN Header */
-/**
-  ******************************************************************************
-  * @file    app_azure_rtos.c
-  * @author  MCD Application Team
-  * @brief   app_azure_rtos application implementation file
-  ******************************************************************************
-    * @attention
-  *
-  * Copyright (c) 2025 STMicroelectronics.
-  * All rights reserved.
-  *
-  * This software is licensed under terms that can be found in the LICENSE file
-  * in the root directory of this software component.
-  * If no LICENSE file comes with this software, it is provided AS-IS.
-  *
-  ******************************************************************************
-  */
-/* USER CODE END Header */
 
-/* Includes ------------------------------------------------------------------*/
 #include "app_azure_rtos.h"
-/* Private includes ----------------------------------------------------------*/
-/* USER CODE BEGIN Includes */
-
-/* USER CODE END Includes */
-
-/* Private typedef -----------------------------------------------------------*/
-/* USER CODE BEGIN PTD */
-
-/* USER CODE END PTD */
-
-/* Private define ------------------------------------------------------------*/
-/* USER CODE BEGIN PD */
-
-/* USER CODE END PD */
-
-/* Private macro -------------------------------------------------------------*/
-/* USER CODE BEGIN PM */
-
-/* USER CODE END PM */
-
-/* Private variables ---------------------------------------------------------*/
-/* USER CODE BEGIN PV */
-
-/* USER CODE END PV */
 
 #if (USE_STATIC_ALLOCATION == 1)
 
-/* USER CODE BEGIN TX_Pool_Buffer */
-/* USER CODE END TX_Pool_Buffer */
+
 #if defined ( __ICCARM__ )
 #pragma data_alignment=4
 #endif
@@ -57,10 +12,6 @@ static TX_BYTE_POOL tx_app_byte_pool;
 
 #endif
 
-/* Private function prototypes -----------------------------------------------*/
-/* USER CODE BEGIN PFP */
-
-/* USER CODE END PFP */
 
   /**
   * @brief  Define the initial system.
@@ -78,29 +29,21 @@ VOID tx_application_define(VOID *first_unused_memory)
 
   if (tx_byte_pool_create(&tx_app_byte_pool, "Tx App memory pool", tx_byte_pool_buffer, TX_APP_MEM_POOL_SIZE) != TX_SUCCESS)
   {
-    /* USER CODE BEGIN TX_Byte_Pool_Error */
-
-    /* USER CODE END TX_Byte_Pool_Error */
   }
   else
   {
-    /* USER CODE BEGIN TX_Byte_Pool_Success */
 
-    /* USER CODE END TX_Byte_Pool_Success */
 
     memory_ptr = (VOID *)&tx_app_byte_pool;
     status = App_ThreadX_Init(memory_ptr);
     if (status != TX_SUCCESS)
     {
-      /* USER CODE BEGIN  App_ThreadX_Init_Error */
       while(1)
       {
       }
-      /* USER CODE END  App_ThreadX_Init_Error */
-    }
-    /* USER CODE BEGIN  App_ThreadX_Init_Success */
 
-    /* USER CODE END  App_ThreadX_Init_Success */
+    }
+
 
   }
 
