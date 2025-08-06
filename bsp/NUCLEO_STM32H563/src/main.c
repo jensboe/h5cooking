40db2379502e52f92ed49c52d850b408816ba8d5
Jens Böckmann <jensboe@outlook.de>
2025-08-06 23:10:36 +0200

    Remove dead code

--- bsp/NUCLEO_STM32H563/src/main.c
+++ bsp/NUCLEO_STM32H563/src/main.c
@@ -112,24 +112,6 @@ int main(void)
 
   MX_ThreadX_Init();
 
-  /* Initialize leds */
-  BSP_LED_Init(LED_GREEN);
-  BSP_LED_Init(LED_YELLOW);
-  BSP_LED_Init(LED_RED);
-
-  /* Initialize USER push-button, will be used to trigger an interrupt each time it's pressed.*/
-  BSP_PB_Init(BUTTON_USER, BUTTON_MODE_EXTI);
-
-  /* Initialize COM1 port (115200, 8 bits (7-bit data + 1 stop bit), no parity */
-  BspCOMInit.BaudRate   = 115200;
-  BspCOMInit.WordLength = COM_WORDLENGTH_8B;
-  BspCOMInit.StopBits   = COM_STOPBITS_1;
-  BspCOMInit.Parity     = COM_PARITY_NONE;
-  BspCOMInit.HwFlowCtl  = COM_HWCONTROL_NONE;
-  if (BSP_COM_Init(COM1, &BspCOMInit) != BSP_ERROR_NONE)
-  {
-    Error_Handler();
-  }
 
   /* We should never get here as control is now taken by the scheduler */
 
